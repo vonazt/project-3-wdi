e4a28241efe86af28cdd1dff8f01106e80c71f8f
Richard Tzanov <richardtzanov@gmail.com>
2018-06-22 16:20:53 +0100

    added owner to record model, changed genre to an array

--- db/seeds.js
+++ db/seeds.js
@@ -1,10 +1,42 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const Record = require('../models/record');
+const User = require('../models/user');
 const { dbURI } = require('../config/environment');
 
 mongoose.connect(dbURI, (err, db) => {
   db.dropDatabase();
+
+  User.create([{
+    username: 'richard',
+    email: 'richard@test.com',
+    password: 'pass',
+    passwordConfirmation: 'pass'
+  }, {
+    username: 'martin',
+    email: 'martin@test.com',
+    password: 'pass',
+    passwordConfirmation: 'pass'
+  }])
+    .then(users => {
+      console.log('users created');
+      return Record.create([{
+        artist: 'David Bowie',
+        title: 'The Man Who Sold The World',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/MWSTWUS2.jpg/220px-MWSTWUS2.jpg',
+        genre: 'Rock',
+        releaseDate: 1970,
+        condition: 'Mint'
+      }, {
+        artist: 'Aphex Twin',
+        title: 'Syro',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/Aphex_Twin_-_Syro_alt_cover.jpg/220px-Aphex_Twin_-_Syro_alt_cover.jpg',
+        genre: 'Electronic',
+        releaseDate: 2014,
+        condition: 'VG'
+        
+      }])
+    })
 
   Record.create({
     artist: 'David Bowie',
