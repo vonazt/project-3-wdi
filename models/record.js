a80edec27e5e30e293c6989997df36bd3c0f5f26
MartinKoeoep <herrkoop@gmail.com>
2018-06-22 15:59:29 +0100

    Added commentSchema to recordmodel

--- models/record.js
+++ models/record.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: true},
+  author: { type: mongoose.Schema.ObjectId, ref: 'User', required: true},
+  rating: { type: Number, min: 1, max: 5}
+});
+
 const recordSchema = new mongoose.Schema({
   artist: { type: String, required: true },
   title: { type: String, required: true },
   image: { type: String, required: true },
   genre: { type: String},
   releaseDate: String,
-  condition: { type: String, required: true }
+  condition: { type: String, required: true },
+  comments: [ commentSchema ]
 });
 
 module.exports = mongoose.model('Record', recordSchema);
