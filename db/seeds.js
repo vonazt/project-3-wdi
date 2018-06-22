6a59bc4b6c4cd1fa8938632e1ad7763d372ab7e7
Richard Tzanov <richardtzanov@gmail.com>
2018-06-22 16:33:24 +0100

    added multiple users and records to seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -19,33 +19,39 @@ mongoose.connect(dbURI, (err, db) => {
     passwordConfirmation: 'pass'
   }])
     .then(users => {
-      console.log('users created');
+      console.log(`${users.length} users created`);
       return Record.create([{
         artist: 'David Bowie',
         title: 'The Man Who Sold The World',
         image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/MWSTWUS2.jpg/220px-MWSTWUS2.jpg',
-        genre: 'Rock',
+        genre: ['Rock'],
         releaseDate: 1970,
-        condition: 'Mint'
+        condition: 'Mint',
+        owner: users[0],
+        comments: [{
+          content: 'great pressing',
+          rating: 4,
+          author: users[1]
+        }, {
+          content: 'not bowie\'s best',
+          rating: 3,
+          author: users[0]
+        }]
       }, {
         artist: 'Aphex Twin',
         title: 'Syro',
         image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/Aphex_Twin_-_Syro_alt_cover.jpg/220px-Aphex_Twin_-_Syro_alt_cover.jpg',
-        genre: 'Electronic',
+        genre: ['Electronic', 'Experimental'],
         releaseDate: 2014,
-        condition: 'VG'
-        
-      }])
+        condition: 'VG',
+        owner: users[1],
+        comments: [{
+          content: 'comes on three vinyl',
+          rating: 5,
+          author: users[0]
+        }]
+      }]);
     })
-
-  Record.create({
-    artist: 'David Bowie',
-    title: 'The Man Who Sold The World',
-    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/MWSTWUS2.jpg/220px-MWSTWUS2.jpg',
-    genre: 'Rock',
-    releaseDate: 1970,
-    condition: 'Mint'
-  })
     .then(records => console.log(`${records.length} record(s) created`))
     .catch(err => console.log(err))
     .finally(() => mongoose.connection.close());
