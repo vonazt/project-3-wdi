049846b20b620871e6b3c0a15fc997ad763e4f20
Richard Tzanov <richardtzanov@gmail.com>
2018-06-22 11:52:02 +0100

    created controllers/records and config/routes folders and files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const { port, dbURI } = require('./config/environment');
 
-
 const mongoose = require('mongoose');
 
 mongoose.connect(dbURI);
