f71f83175f804ac2a0dc9f9aa623a75e58d470d8
Richard Tzanov <richardtzanov@gmail.com>
2018-06-22 13:57:24 +0100

    written secureRoute in lib /

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,5 @@
 const port = 4000;
 const dbURI = 'mongodb://localhost/tall-man-records';
+const secret = 'rahLxf,y8$KAv23:';
 
-module.exports = { port, dbURI };
+module.exports = { port, dbURI, secret };
