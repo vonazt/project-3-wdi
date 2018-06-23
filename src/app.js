4df3e59c39635f6836222c7f007b770792639acb
Richard Tzanov <richardtzanov@gmail.com>
2018-06-23 09:59:27 +0100

    attached AuthRegisterCtrl etc to app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
+import 'satellizer';
 
 import 'bulma';
 
 import Router from './config/routes';
+import Auth from './config/satellizer';
 
 import MainCtrl from './controllers/main';
+import AuthRegisterCtrl from './controllers/auth/register';
 
-angular.module('tallManRecordsApi', ['ui.router'])
+angular.module('tallManRecordsApi', ['ui.router', 'satellizer'])
   .config(Router)
-  .controller('MainCtrl', MainCtrl);
+  .config(Auth)
+  .controller('MainCtrl', MainCtrl)
+  .controller('AuthRegisterCtrl', AuthRegisterCtrl);
