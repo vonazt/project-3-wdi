57dde18efe37b7dfdb2ac17cd667854973b275b8
Richard Tzanov <richardtzanov@gmail.com>
2018-06-24 11:21:38 +0100

    created wishlist dropdown menu and offers placeholder

--- src/controllers/profiles/show.js
+++ src/controllers/profiles/show.js
@@ -4,7 +4,10 @@ function ProfilesShowCtrl($scope, $http, $state) {
     method: 'GET',
     url: `/api/profiles/${$state.params.id}`
   })
-    .then(res => $scope.user = res.data);
+    .then(res => {
+      $scope.user = res.data;
+
+    });
 }
 
 export default ProfilesShowCtrl;
