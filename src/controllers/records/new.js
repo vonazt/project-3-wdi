04d3f140edf365679044dfa4197f3f279959a943
Richard Tzanov <richardtzanov@gmail.com>
2018-06-24 19:34:47 +0100

    selecting genres will push them into an array

--- src/controllers/records/new.js
+++ src/controllers/records/new.js
@@ -1,7 +1,14 @@
 function RecordsNewCtrl($scope, $state, $http) {
   $scope.data = {};
+  $scope.genreArray = [];
+
+  $scope.addGenre = function() {
+    $scope.genreArray.push($scope.data.genre);
+    $scope.data.genre = '';
+  };
 
   $scope.createRecord = function() {
+    if($scope.genreArray.length > 0) $scope.data.genre = $scope.genreArray;
     $http({
       method: 'POST',
       url: '/api/records',
