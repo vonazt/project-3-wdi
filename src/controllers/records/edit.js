52246a02fb721f9d970bf1a47b969441c62936d8
Richard Tzanov <richardtzanov@gmail.com>
2018-06-24 20:11:02 +0100

    Genres populate properly and can be added and removed in edit.html

--- src/controllers/records/edit.js
+++ src/controllers/records/edit.js
@@ -1,6 +1,21 @@
 function RecordsEditCtrl($scope, $state, $http) {
+  $scope.genreArray = [];
+  $scope.showGenreArray = false;
+
+  $scope.addGenre = function() {
+    $scope.genreArray.push($scope.data.genre);
+    $scope.showGenreArray = true;
+    $scope.data.genre = '';
+  };
+
+  $scope.removeGenre = function(e) {
+    const index = $scope.genreArray.indexOf(e);
+    $scope.genreArray.splice(index, 1);
+    $scope.data.genre = '';
+  };
 
   $scope.updateRecord = function() {
+    $scope.data.genre = $scope.genreArray;
     $http({
       method: 'PUT',
       url: `/api/records/${$state.params.id}`,
@@ -13,7 +28,12 @@ function RecordsEditCtrl($scope, $state, $http) {
     method: 'GET',
     url: `/api/records/${$state.params.id}`
   })
-    .then(res => $scope.data = res.data);
+    .then(res => {
+      $scope.data = res.data;
+      res.data.genre.forEach(item => $scope.genreArray.push(item));
+      $scope.showGenreArray = true;
+      $scope.data.genre = '';
+    });
 }
 
 export default RecordsEditCtrl;
