06dfa95bbb651d50d6129c9e09ac065590d4c33d
Richard Tzanov <richardtzanov@gmail.com>
2018-06-26 16:38:31 +0100

    adding multiple records to offer needs fixing in user model

--- src/controllers/requests/new.js
+++ src/controllers/requests/new.js
@@ -12,8 +12,7 @@ function RequestsNewCtrl($scope, $state, $http, $auth) {
   };
 
   $scope.submitRequest = function(currentUserId) {
-    $scope.data.offeredRecord = offerArray;
-    console.log($scope.data.offeredRecord);
+    $scope.data.offeredRecord = offerArray[0];
     $http({
       method: 'POST',
       url: `/api/records/${$state.params.id}/requests`,
