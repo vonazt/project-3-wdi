e430cd2c74ad77f36bfc249b4f2a685d4437d30e
MartinKoeoep <herrkoop@gmail.com>
2018-06-28 12:48:51 +0100

    show_spec.js for requests done.

--- test/requests/show_spec.js
+++ test/requests/show_spec.js
@@ -56,28 +56,48 @@ describe('GET /requests/:id', () => {
         requestData.wantedRecord = request.wantedRecord;
         requestData.offeredRecord = request.offeredRecord;
         requestData.message = request.message;
+        requestData._id = request._id;
         requestId = request._id;
         done();
       });
   });
 
   it('should return a 401 response without a token', done => {
-    api.post(`/api/requests/${recordOneId}`)
+    api.get(`/api/requests/${requestId}`)
       .end((err, res) => {
         expect(res.status).to.eq(401);
         done();
       });
   });
 
-  it('should return a 201 response', done => {
-    api.post(`/api/requests/${recordOneId}`)
+  it('should return a 200 response', done => {
+    api.get(`/api/requests/${requestId}`)
       .set('Authorization', `Bearer ${token}`)
       .send(requestData)
       .end((err, res) => {
-        expect(res.status).to.eq(201);
+        expect(res.status).to.eq(200);
         done();
       });
   });
 
+  it('should return an object', done => {
+    api.get(`/api/requests/${requestId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+
+  it('should return the correct data', done => {
+    api.get(`/api/requests/${requestId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res.body.wantedRecord._id).to.eq(requestData.wantedRecord.toString());
+        expect(res.body.offeredRecord[0]._id).to.eq(requestData.offeredRecord[0].toString());
+        expect(res.body.message).to.eq(requestData.message);
+        done();
+      });
+  });
 
 });
