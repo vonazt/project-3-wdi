43c32cceefcc0b9a93915f93d3d1cac57cc40b4c
Richard Tzanov <richardtzanov@gmail.com>
2018-06-29 10:44:39 +0100

    made app mobile responsive. ish.

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,11 +1,13 @@
 function MainCtrl($scope, $state, $auth, $transitions, $rootScope, $timeout) {
   $scope.isAuthenticated = $auth.isAuthenticated;
+  $scope.navbarOpen = false;
 
   $scope.getUserId = function() {
     return $scope.currentUserId = $auth.getPayload().sub;
   };
 
   $transitions.onSuccess({}, () => {
+    $scope.navbarOpen = false;
     $scope.isHomepage = $state.$current.name === 'home';
   });
 
@@ -16,6 +18,10 @@ function MainCtrl($scope, $state, $auth, $transitions, $rootScope, $timeout) {
       $scope.flashMessage = null;
     }, 3000);
   });
+
+  $scope.toggleMenu = function() {
+    $scope.navbarOpen = !$scope.navbarOpen;
+  };
 
   $scope.logout = function() {
     $auth.logout();
